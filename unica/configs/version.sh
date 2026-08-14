ae217a4c84fc4fd5453940d0411b0ede9f9ff1fb
ohhfrman <dakata.iordanov21@gmail.com>
2026-08-14 09:28:09 +0300

    Initial 4.0.0

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Only the below variable(s) need to be changed!
-VERSION_MAJOR=3
-VERSION_MINOR=1
+VERSION_MAJOR=4
+VERSION_MINOR=0
 VERSION_PATCH=0
 
 # The below variables will be generated automatically
